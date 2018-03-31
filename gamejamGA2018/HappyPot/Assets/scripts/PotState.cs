5f166f10c3a5d1b0593c5f83e29ebd95dcc1dd3d
Unknown <alexis.martial16@gmail.com>
2018-03-31 23:07:42 +0200

    take it bro

---
+++ gamejamGA2018/HappyPot/Assets/scripts/PotState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotState : MonoBehaviour {
+	//moveState
+	enum MoveState {iddle, walk, run};
+	//s1 iddle
+	//s2 walk
+	//s3 run
+
+	//fearState
+	//fear
+	//Happy
+	private bool isFeared;
+
+	//growUpState
+		//small
+		//smallToBig
+		//big
+		//bigToSmall
+		
+		private Rigidbody2D m_physic;
+		private int m_direction;//left -1, right 1, else 0
+		private Gravity m_gravity;
+		private Transform m_transform;
+	// Use this for initialization
+	void Start () {
+		isFeared = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
