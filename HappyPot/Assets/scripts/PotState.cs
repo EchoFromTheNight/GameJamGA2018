ae91d4052ce1f16f928957b7cf9ccb5366036bef
Unknown <alexis.martial16@gmail.com>
2018-04-01 15:23:50 +0200

    update potState

--- HappyPot/Assets/scripts/PotState.cs
+++ HappyPot/Assets/scripts/PotState.cs
@@ -5,32 +5,67 @@ using UnityEngine;
 public class PotState : MonoBehaviour {
 	//moveState
 	enum MoveState {iddle, walk, run};
-	//s1 iddle
-	//s2 walk
-	//s3 run
-
 	//fearState
-	//fear
-	//Happy
-	private bool isFeared;
-
+	enum HumorState {happy, fear};
 	//growUpState
-		//small
-		//smallToBig
-		//big
-		//bigToSmall
-		
-		private Rigidbody2D m_physic;
-		private int m_direction;//left -1, right 1, else 0
-		private Gravity m_gravity;
-		private Transform m_transform;
+	enum GrowUpState{small, smallToBig, big, bigToSmall};
+	//states
+	private MoveState moveState;
+	private HumorState humorState;
+	private GrowUpState growUpState;
+	private int direction;//left -1, right 1, else 0
+
+	//public
+	public float runSpeed = 0;
+	//private Rigidbody2D m_physic;
+	private Transform transform;
+	private Rigidbody2D physic;
+
 	// Use this for initialization
 	void Start () {
-		isFeared = false;
+		moveState = MoveState.iddle;
+		humorState = HumorState.happy;
+		growUpState = GrowUpState.small;
+		direction = 0;
+		transform = gameObject.GetComponent<Transform> ();
+		physic = gameObject.GetComponent<Rigidbody2D> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
+		setAnimations();
+		setSound();
+		setMove();
+
+		//handle Move animation
+		/*if(physic.velocity.distance == 0 ){
+			moveState = MoveState.iddle;
+		}else if(physic.velocity.value > runSpeed){
+			moveState = MoveState.run;
+		}else{
+			moveState = MoveState.walk;
+		}*/
+
+	}
+	//external functions to set the state
+	public void seeFire(Vector2 position){
+		//fear + force in oposite direction
+		humorState = HumorState.fear;
+		//Vector2 tmp = transform.position -position;
+	}
+	public void unseeFire(){
+		//happy + state + stop
+	}
+	public void seeLight(Vector2 position){
+		//happy + force in same direction
+	}
+	private void setAnimations(){
+
+	}
+	private void setMove(){
+
+	}
+	private void setSound(){
 
 	}
 }
