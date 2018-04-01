3f9df8f178fcc39267042d758856d5ee39a33e70
Unknown <edgar1893@gmail.com>
2018-04-01 14:28:33 +0200

    SwitchController
    Création de switch controller avec un script extèrne pour éxecuter une action.

---
+++ HappyPot/Assets/scripts/ExternalAction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalAction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public virtual void ExecuteAction(bool isOn)
+    {
+        //If switch is on
+        if (isOn)
+        {
+            Debug.Log("Light on");
+        }
+        else   //if switch is off
+        {
+            Debug.Log("Light off");
+        }
+    }
+}
---
+++ HappyPot/Assets/scripts/SwitchController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour {
+
+    //State of the switch
+    public bool isOn;
+
+    //Light object that will be affected by the switch
+    public GameObject externalObject;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    private void OnMouseDown()
+    {
+        isOn = !isOn;
+        ExternalAction script = externalObject.GetComponent<ExternalAction>();
+        script.ExecuteAction(isOn);
+    }   
+}
