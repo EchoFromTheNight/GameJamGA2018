ceb62ed13555c4abe4a8cdde61dab9097b09645b
Unknown <edgar1893@gmail.com>
2018-04-01 15:23:16 +0200

    SwitchController fix

--- HappyPot/Assets/scripts/PotState.cs
+++ HappyPot/Assets/scripts/PotState.cs
@@ -22,7 +22,7 @@ public class PotState : MonoBehaviour {
 		
 		private Rigidbody2D m_physic;
 		private int m_direction;//left -1, right 1, else 0
-		private Gravity m_gravity;
+		//private Gravity m_gravity;
 		private Transform m_transform;
 	// Use this for initialization
 	void Start () {
