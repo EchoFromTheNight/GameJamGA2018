e9d9930627bed5a1241cd510cd4f187332e0678d
Edgar Lopez <edgar1893@gmail.com>
2018-04-02 12:23:06 +0200

    EndGame Scene
    Creation de la scene de fin

---
+++ HappyPot/Assets/scripts/AnimationEndController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEndController : MonoBehaviour {
+
+    public GameObject animation1;
+
+    public GameObject animation2;
+
+    // Use this for initialization
+    void Start() {
+        animation2.SetActive(false);
+        StartCoroutine("WaitAnimation1");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator WaitAnimation1()
+    {
+        yield return new WaitForSeconds(4);
+        animation1.SetActive(false);
+        animation2.SetActive(true);
+    }
+}
