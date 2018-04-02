d516dd4aa893e54c71c0b0e73a88d85ca6c9dd5f
Edgar Lopez <edgar1893@gmail.com>
2018-04-02 13:48:14 +0200

    Last Commit
    Last Commit

--- HappyPot/Assets/scripts/AnimationEndController.cs
+++ HappyPot/Assets/scripts/AnimationEndController.cs
@@ -7,8 +7,14 @@ public class AnimationEndController : MonoBehaviour {
     //AudioClip
     public AudioClip audioClip;
 
+    //AudioClip
+    public AudioClip audioClip2;
+
     //Reference to the AudioSource
     public AudioSource audioSource;
+
+    //Reference to the AudioSource
+    public AudioSource audioSource2;
 
     public GameObject animation1;
 
@@ -17,9 +23,11 @@ public class AnimationEndController : MonoBehaviour {
     // Use this for initialization
     void Start() {
         audioSource.clip = audioClip;
+        audioSource2.clip = audioClip2;
         animation2.SetActive(false);
         audioSource.Play();
         StartCoroutine("WaitAnimation1");
+        audioSource2.Play();
     }
 	
 	// Update is called once per frame
